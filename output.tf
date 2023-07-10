6a45b5fd2c289d037c48478ab4bf7834596ea50f
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2023-07-10 12:09:14 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -14,7 +14,15 @@ output "tfe_appplication" {
   value = "https://${var.dns_hostname}.${var.dns_zonename}"
 }
 
-output "ssh_tfe_server" {
-  value = "ssh -J ubuntu@${var.dns_hostname}-client.${var.dns_zonename} ubuntu@<internal ip address of the TFE server>"
+data "aws_instances" "foo" {
+  instance_tags = {
+    "Name" = "${var.tag_prefix}-tfe-asg"
+  }
+  instance_state_names = ["running"]
 }
 
+output "ssh_tfe_server" {
+  value = [
+    for k in data.aws_instances.foo.private_ips : "ssh -J ubuntu@${var.dns_hostname}-client.${var.dns_zonename} ubuntu@${k}"
+  ]
+}
