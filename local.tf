551517ca6a95f992841b0fc88d53d8f846ded470
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2023-07-11 09:26:36 +0200

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,6 +1,5 @@
 locals {
-  az1 = "${var.region}a"
-  az2 = "${var.region}b"
+  az1       = "${var.region}a"
+  az2       = "${var.region}b"
   tfe_setup = var.tfe_active_active ? aws_launch_configuration.as_conf_tfe_active.name : aws_launch_configuration.as_conf_tfe_single.name
 }
-
