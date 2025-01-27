64481f60dabea21a27e86ecab24c5f04b3ac4808
kikitux <alvaro@hashicorp.com>
2025-01-27 11:59:36 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -131,13 +131,13 @@ resource "aws_security_group" "tfe_server_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-#  ingress {
-#    description = "https from internet"
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+  #  ingress {
+  #    description = "https from internet"
+  #    from_port   = 22
+  #    to_port     = 22
+  #    protocol    = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
 
   ingress {
     description = "netdata from internet"
